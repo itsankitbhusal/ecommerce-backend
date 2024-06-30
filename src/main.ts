1ddd9f232ce0721c64d3efa3fc90feeaafa8452b
itsankitbhusal <ankitbhusal95@gmail.com>
2024-06-30 12:41:05 +0545

    global response format

--- src/main.ts
+++ src/main.ts
@@ -6,11 +6,13 @@ import {
   INestApplication,
   ValidationPipe,
 } from '@nestjs/common';
+import { ResponseInterceptor } from './response/response.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.useGlobalInterceptors(new ResponseInterceptor());
 
   const config = new DocumentBuilder()
     .setTitle('Ecommerce REST API')
