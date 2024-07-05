e1cea4fbbf7b73aa83b9fbd275708de86c29644e
itsankitbhusal <ankitbhusal95@gmail.com>
2024-07-05 17:57:03 +0545

    docker compose for super token

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,9 +11,15 @@ import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
+    PrismaModule,
+    UsersModule,
+    CategoriesModule,
+    ProductsModule,
+    BannersModule,
+    OrdersModule,
     AuthModule.forRoot({
       // https://try.supertokens.com is for demo purposes. Replace this with the address of your core instance (sign up on supertokens.com), or self host a core.
-      connectionURI: 'https://try.supertokens.com',
+      connectionURI: 'http://localhost:3567',
       // apiKey: <API_KEY(if configured)>,
       appInfo: {
         // Learn more about this on https://supertokens.com/docs/passwordless/appinfo
